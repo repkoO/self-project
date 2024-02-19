07ad11ed618b8c914e40f233eb7f49cdb569c4d9
Kirill Medvedev <repkomail@gmail.com>
2024-02-19 22:03:39 +0200

    fix: fix model

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const modalWrapper = document.querySelector('.modal__wrapper');
 const cardList = document.querySelectorAll('.img__item')
 
 const generateModalWindow = (item) =>`<div class="modal__container">
+  <div class="modal__close">
+    <svg class="close__svg" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="30px" height="30px" viewBox="0 0 41.43 41.43" xml:space="preserve" transform="rotate(70)"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M27.768,0H13.662c-0.345,0-0.625,0.28-0.625,0.625v12.413H0.625C0.28,13.038,0,13.318,0,13.663v14.104 c0,0.346,0.28,0.625,0.625,0.625h12.412v12.414c0,0.346,0.28,0.625,0.625,0.625h14.105c0.346,0,0.625-0.279,0.625-0.625V28.393 h12.414c0.346,0,0.625-0.279,0.625-0.625V13.663c0-0.345-0.279-0.625-0.625-0.625H28.393V0.625C28.393,0.281,28.113,0,27.768,0z M40.182,14.288v12.854H27.768c-0.344,0-0.625,0.281-0.625,0.625v12.414H14.287V27.768c0-0.344-0.28-0.625-0.625-0.625H1.25V14.288 h12.412c0.345,0,0.625-0.28,0.625-0.625V1.25h12.855v12.413c0,0.345,0.281,0.625,0.625,0.625H40.182z"></path> </g> </g></svg>
+  </div>
   <div class="modal__img">
     <img src="${item.img}" alt="">
   </div>
@@ -15,12 +18,8 @@ const generateModalWindow = (item) =>`<div class="modal__container">
     <div class="modal__link">
         <a href="${item.link}">Ссылка на проект</a>
     </div>
-    <div class="modal__close">
-    <svg class="close__svg" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="30px" height="30px" viewBox="0 0 41.43 41.43" xml:space="preserve" transform="rotate(45)"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M27.768,0H13.662c-0.345,0-0.625,0.28-0.625,0.625v12.413H0.625C0.28,13.038,0,13.318,0,13.663v14.104 c0,0.346,0.28,0.625,0.625,0.625h12.412v12.414c0,0.346,0.28,0.625,0.625,0.625h14.105c0.346,0,0.625-0.279,0.625-0.625V28.393 h12.414c0.346,0,0.625-0.279,0.625-0.625V13.663c0-0.345-0.279-0.625-0.625-0.625H28.393V0.625C28.393,0.281,28.113,0,27.768,0z M40.182,14.288v12.854H27.768c-0.344,0-0.625,0.281-0.625,0.625v12.414H14.287V27.768c0-0.344-0.28-0.625-0.625-0.625H1.25V14.288 h12.412c0.345,0,0.625-0.28,0.625-0.625V1.25h12.855v12.413c0,0.345,0.281,0.625,0.625,0.625H40.182z"></path> </g> </g></svg>    </div>
   </div>
 </div>`
-
-
 
 const getData = async () => {
    const res = await fetch('db.json');
